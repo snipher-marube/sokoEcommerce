4fd1b7f397e544b98fc9fc03044366668ed9fa8b
Snipher Marube <sniphermarube@gmail.com>
2023-11-17 16:14:28 +0300

    Product variations complete

--- carts/views.py
+++ carts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.http import HttpResponse
 
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -14,6 +14,19 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id) # get the product
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+            
+    
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
     except Cart.DoesNotExist:
--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from .models import Product
+from .models import Product, Variation
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -11,3 +11,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['thumbnail', 'product_name', 'price', 'stock', 'category', 'created_date', 'modified_date', 'is_available']
     prepopulated_fields = {'slug': ('product_name',)}
     list_display_links = ['thumbnail', 'product_name']
+
+@admin.register(Variation)
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ['product', 'variation_category', 'variation_value', 'is_active', 'created_date']
+    list_editable = ['is_active']
+    list_filter = ['product', 'variation_category', 'variation_value', 'is_active', 'created_date']
+    list_display_links = ['product', 'variation_value']
