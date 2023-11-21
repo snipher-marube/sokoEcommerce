9b863ad91cecea3bfc1231b5a7ded280a7fcc231
Snipher Marube <sniphermarube@gmail.com>
2023-11-21 07:50:29 +0300

    accounts registration preparations

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def registerUser(request):
+    return render(request, 'accounts/register.html')
+
+def loginUser(request):
+    return render(request, 'accounts/login.html')
