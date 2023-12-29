81362a68009ad8c37368283f02ef3645fedc8f69
Snipher Marube <sniphermarube@gmail.com>
2023-12-29 16:34:39 +0300

    Review Order Payment Page

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,9 @@ from carts.models import Cart, CartItem
 from .forms import OrderForm
 from .models import Order, Payment, OrderProduct
 
+def payments(request):
+    return render(request, 'orders/payments.html')
+
 def place_order(request, total=0, quantity=0):
     current_user = request.user
     # if cart count is less than or equal to 0, redirect back to shop
@@ -55,7 +58,17 @@ def place_order(request, total=0, quantity=0):
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
+
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
             
-            return redirect('checkout')
+            return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
