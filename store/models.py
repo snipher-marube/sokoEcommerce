48536cc5094e681af09a7650cfac4cb857f5738f
Snipher Marube <sniphermarube@gmail.com>
2024-01-06 19:33:27 +0300

    Rating Average Stars & Review Counter

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 from category.models import Category
 from accounts.models import Account
 
@@ -21,6 +22,15 @@ class Product(models.Model):
     
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
+    
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True)
+        average = reviews.aggregate(average=models.Avg('rating'))
+        avg = 0
+        if average['average'] is not None:
+            avg = float(average['average'])
+        return avg
+
 class VariationManager(models.Manager):
     def colors(self):
         return super(VariationManager, self).filter(variation_category='color', is_active=True)
