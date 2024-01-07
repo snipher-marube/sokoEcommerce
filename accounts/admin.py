c89a885960e4cdb3b967b395e5ed5cba5439dd79
Snipher Marube <sniphermarube@gmail.com>
2024-01-07 11:48:59 +0300

    User Profile Model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from django.utils.html import format_html
+from .models import Account, UserProfile
 
 @admin.register(Account)
 class AccountAdmin(UserAdmin):
@@ -9,6 +10,22 @@ class AccountAdmin(UserAdmin):
     readonly_fields = ('date_joined', 'last_login')
     list_display_links = ('email', 'username', 'first_name', 'last_name')
     ordering = ('-date_joined',)
+    
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    # display user profile image thumbnail in admin
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.profile_picture.url))
+
+
+    list_display = ('thumbnail', 'user', 'city', 'county',  'address_line_1', 'address_line_2')
+    search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name', 'phone_number')
+    list_display_links = ('user',)
+    ordering = ('-user',)
     
     filter_horizontal = ()
     list_filter = ()
