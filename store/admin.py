20c8394045519a9558b2ed3c11eeb61bb422d314
Snipher Marube <sniphermarube@gmail.com>
2024-01-07 18:29:29 +0300

    Product Gallery Implementation

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.utils.html import format_html
+import admin_thumbnails 
 
-from .models import Product, Variation, ReviewRating
+from .models import Product, Variation, ReviewRating, ProductGallery
+
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryAdmin(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -11,6 +17,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['thumbnail', 'product_name', 'price', 'stock', 'category', 'created_date', 'modified_date', 'is_available']
     prepopulated_fields = {'slug': ('product_name',)}
     list_display_links = ['thumbnail', 'product_name']
+
+    inlines = [ProductGalleryAdmin]
 
 @admin.register(Variation)
 class VariationAdmin(admin.ModelAdmin):
@@ -25,3 +33,15 @@ class ReviewRatingAdmin(admin.ModelAdmin):
     list_filter = ['product', 'user', 'rating', 'status', 'created_at']
     list_editable = ['status']
     list_display_links = ['product', 'user', 'subject']
+
+
+
+@admin.register(ProductGallery)
+class ProductGalleryAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.image.url))
+    
+    list_display = ['thumbnail', 'product']
+    list_display_links = ['thumbnail', 'product']
+
+    
