ce2c1d43e9519a154cd0d3b9ac9ab474e74d9907
Snipher Marube <sniphermarube@gmail.com>
2024-01-24 02:57:30 +0300

    new

--- sokoMarket/settings.py
+++ sokoMarket/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'axes',
     'appointment.apps.AppointmentConfig', # new
    
+   
 ]
 
 MIDDLEWARE = [
@@ -139,6 +140,9 @@ EMAIL_PORT = config("EMAIL_PORT", cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+
+
 
 
 # Custom user model
